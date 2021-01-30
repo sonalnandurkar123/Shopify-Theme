d63fc17e3c33135ed0ee2f47b59c510786959a03
sonal <sonalnandurkar988@gmail.com>
2021-01-30 08:20:08 +0530

    shopify theme

---
+++ assets/theme.js
@@ -0,0 +1,10 @@
+(function () {
+  'use strict';
+
+  var example = function example() {
+    console.info('This is an example to show the use of import/export syntax.');
+  };
+
+  example();
+})();
+//# sourceMappingURL=theme.js.map
